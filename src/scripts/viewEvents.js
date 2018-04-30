9f053ca36640870998476fb48f61effde3660e2e
Levi Schubert <me@levischubert.com>
2018-04-30 15:22:20 -0500

    added event handlers for forms and to change views

---
+++ src/scripts/viewEvents.js
@@ -0,0 +1,52 @@
+const openTaskView = () => {
+	let view = document.querySelector("#tasks")
+	let closeOne = document.querySelector("#charts")
+	let closeTwo = document.querySelector("#archive")
+	if(view.classList.contains("hide")){
+		view.classList.toggle("hide")
+	}
+	if(!closeOne.classList.contains("hide")){
+		closeOne.classList.toggle("hide")
+	}
+	if(!closeTwo.classList.contains("hide")){
+		closeTwo.classList.toggle("hide")
+	}
+}
+
+
+const openChartsView = () => {
+	let closeOne = document.querySelector("#tasks")
+	let view = document.querySelector("#charts")
+	let closeTwo = document.querySelector("#archive")
+	if(view.classList.contains("hide")){
+		view.classList.toggle("hide")
+	}
+	if(!closeOne.classList.contains("hide")){
+		closeOne.classList.toggle("hide")
+	}
+	if(!closeTwo.classList.contains("hide")){
+		closeTwo.classList.toggle("hide")
+	}
+}
+
+
+const openArchiveView = () => {
+	let closeTwo = document.querySelector("#tasks")
+	let closeOne = document.querySelector("#charts")
+	let view = document.querySelector("#archive")
+	if(view.classList.contains("hide")){
+		view.classList.toggle("hide")
+	}
+	if(!closeOne.classList.contains("hide")){
+		closeOne.classList.toggle("hide")
+	}
+	if(!closeTwo.classList.contains("hide")){
+		closeTwo.classList.toggle("hide")
+	}
+}
+
+const openTask = document.querySelector("#tasks__view").addEventListener("click", openTaskView);
+
+const openCharts = document.querySelector("#charts__view").addEventListener("click", openChartsView);
+
+const openArchive = document.querySelector("#archive__view").addEventListener("click", openArchiveView);
