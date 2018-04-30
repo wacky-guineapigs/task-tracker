6a17507e62be82ef99beee4a85c7378ddf8f0221
joshdbarton <joshdbarton@gmail.com>
2018-04-30 13:37:46 -0500

    added db manager actor to create db and save new tasks

---
+++ src/scripts/JDBnewTaskFunction.js
@@ -0,0 +1,52 @@
+const manageDB = Object.create({}, {
+    tasks: {
+        enumerable: true, 
+        writable: true, 
+        value: []
+    },
+    saveNewTask: {
+        enumerable: true, 
+        writable: false, 
+        value: (title, description, category, due) => {
+            const newTask = Object.create(null, {
+                Title: {
+                    enumerable: true, 
+                    writable: true, 
+                    value: title
+                },
+                Description:{
+                    enumerable: true, 
+                    writable: true, 
+                    value: description
+                },
+                Category:{
+                    enumerable: true, 
+                    writable: true, 
+                    value: category
+                }, 
+                Archived:{
+                    enumerable: true, 
+                    writable: true, 
+                    value: false
+                },
+                Created: {
+                    enumerable: true, 
+                    writable: true, 
+                    value: new Date()
+                }, 
+                Completed: {
+                    enumerable: true, 
+                    writable: true, 
+                    value: ""
+                }, 
+                Due: {
+                    enumerable: true, 
+                    writable: true, 
+                    value: due
+                }
+            })
+            tasks.push(newTask)
+            localStorage.setItem("tasks", tasks)
+        }
+    }
+})
