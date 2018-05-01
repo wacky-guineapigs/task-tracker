35bfded588dd5433ea492131a5e67ea788f7ccf3
joshdbarton <joshdbarton@gmail.com>
2018-05-01 12:10:56 -0500

    created dataAnalysis actor object

--- src/scripts/graphs.js
+++ src/scripts/graphs.js
@@ -2,60 +2,70 @@ const manageDB = require("./manageDB")
 const categoriesDB = require("./categories")
 
 
-///How many tasks currently in each active column and how many are archived
-///data for the first chart
-const currentColumns = []
-currentColumns.push(manageDB.tasks.filter(task => task.currentStatus === "todo").length)
-currentColumns.push(manageDB.tasks.filter(task => task.currentStatus === "doing").length)
-currentColumns.push(manageDB.tasks.filter(task => task.currentStatus === "done").length)
-currentColumns.push(manageDB.tasks.filter(task => task.currentStatus === "archived").length)
-
-///How many tasks in each category (include uncategorized)
-const categories = {}
-categoriesDB.categories.forEach(category => {
-    categoriesTally[category] = 0
+///dataAnalysis methods return arrays for the datasets of the charts
+const dataAnalysis = Object.create({}, {
+    currentColumns: {
+        enumerable: true, 
+        writable: false, 
+        value: () => {
+            const currentColumns = []
+            currentColumns.push(manageDB.tasks.filter(task => task.currentStatus === "todo").length)
+            currentColumns.push(manageDB.tasks.filter(task => task.currentStatus === "doing").length)
+            currentColumns.push(manageDB.tasks.filter(task => task.currentStatus === "done").length)
+            currentColumns.push(manageDB.tasks.filter(task => task.currentStatus === "archived").length)
+            return currentColumns
+        }
+    },
+    currentCategoryTotals: {
+        enumerable: true, 
+        writable: false, 
+        value: (tallyOrlabels) => {
+            const categories = {}
+            categoriesDB.categories.forEach(category => {
+                categoriesTally[category] = 0
+            })
+            manageDB.tasks.forEach(task => {
+                categoriesTally[task.Category]++
+            })
+            const catTallySortable = categoriesTally.entries()
+            const tally = []
+            const labels = []
+            catTallySortable.forEach(category => {
+                tally.push(category[1])
+                labels.push(category[0])
+            return tallyOrlabels
+        })
+        }
+    },
+    onTimeData: {
+        enumerable: true, 
+        writable: false, 
+        value: () => {
+            const onTimeTasks = manageDB.tasks.filter(task => {
+                const dueDate = parse(new Date(task.Due))
+                const completedDate = parse(task.Completed)
+                return dueDate >= completedDate
+            })
+            const lateTasks = manageDB.tasks.filter(task => {
+                const dueDate = parse(new Date(task.Due))
+                const completedDate = parse(task.Completed)
+                return dueDate <= completedDate
+            })
+            const onTime = onTimeTasks.length
+            const behindSchedule = lateTasks.length
+            const onTimeChartData = []
+            onTimeChartData.push(onTime)
+            onTimeChartData.push(behindSchedule)
+            return onTimeChartData
+        }
+    },
+    categoryAverages: {
+        enumerable: true, 
+        writable: false, 
+        value: () => {
+            return categoriesDB.categories.map(category => {
+            return manageDB.tasks.filter(task => task.Category === category).reduce((a, b) => {a + (parse(b.Completed) - parse(b.Created))}, 0)/manageDB.tasks.filter(task => task.Category === category)
+            })
+        }
+    }
 })
-manageDB.tasks.forEach(task => {
-    categoriesTally[task.Category]++
-})
-const catTallySortable = categoriesTally.entries()
-//data and labels for the second chart
-const tallySorted = []
-const tallyLabelsSorted = []
-catTallySortable.forEach(category => {
-    tallySorted.push(category[1])
-    tallyLabelsSorted.push(category[0])
-})
-
-///How many tasks were completed on time vs. how many were not
-const onTimeTasks = manageDB.tasks.filter(task => {
-    const dueDate = parse(new Date(task.Due))
-    const completedDate = parse(task.Completed)
-    return dueDate >= completedDate
-})
-const lateTasks = manageDB.tasks.filter(task => {
-    const dueDate = parse(new Date(task.Due))
-    const completedDate = parse(task.Completed)
-    return dueDate <= completedDate
-})
-
-const onTime = onTimeTasks.length
-const behindSchedule = lateTasks.length
-
-const onTimeChartData = []
-onTimeChartData.push(onTime)
-onTimeChartData.push(behindSchedule)
-
-
-///Average time it took to complete tasks in each category
-const categoryAverages = categoriesDB.categories.map(category => {
-     return manageDB.tasks.filter(task => task.Category === category).reduce((a, b) => {
-        a + (parse(b.Completed) - parse(b.Created))
-     }, 0)/manageDB.tasks.filter(task => task.Category === category)
-})
-
-///labels for fourth graph:
-categoriesDB.categories
-///data for fourth graph: 
-categoryAverages
-
