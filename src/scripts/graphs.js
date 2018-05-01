b1be78170ee4a350df43b6c605e2d5afaf42d20f
joshdbarton <joshdbarton@gmail.com>
2018-05-01 10:16:59 -0500

    first commit

---
+++ src/scripts/graphs.js
@@ -0,0 +1,50 @@
+const manageDB = require("./manageDB")
+const categoriesDB = require("./categories")
+
+
+///How many tasks currently in each active column and how many are archived
+///data for the first chart
+const currentColumns = []
+currentColumns.push(manageDB.tasks.filter(task => task.currentStatus === "todo").length)
+currentColumns.push(manageDB.tasks.filter(task => task.currentStatus === "doing").length)
+currentColumns.push(manageDB.tasks.filter(task => task.currentStatus === "done").length)
+currentColumns.push(manageDB.tasks.filter(task => task.currentStatus === "archived").length)
+
+///How many tasks in each category (include uncategorized)
+const categoriesTally = {}
+categoriesDB.categories.forEach(category => {
+    categoriesTally[category] = 0
+})
+manageDB.tasks.forEach(task => {
+    categoriesTally[task.Category]++
+})
+const catTallySortable = categoriesTally.entries()
+//data and labels for the second chart
+const tallySorted = []
+const tallyLabelsSorted = []
+catTallySortable.forEach(category => {
+    tallySorted.push(category[1])
+    tallyLabelsSorted.push(category[0])
+})
+
+///How many tasks were completed on time vs. how many were not
+const onTimeTasks = manageDB.tasks.filter(task => {
+    const dueDate = parse(new Date(task.Due))
+    const completedDate = parse(task.Completed)
+    return dueDate >= completedDate
+})
+const lateTasks = manageDB.tasks.filter(task => {
+    const dueDate = parse(new Date(task.Due))
+    const completedDate = parse(task.Completed)
+    return dueDate <= completedDate
+})
+
+const onTime = onTimeTasks.length
+const behindSchedule = lateTasks.length
+
+const onTimeChartData = []
+onTimeChartData.push(onTime)
+onTimeChartData.push(behindSchedule)
+
+
+///Average time it took to complete tasks in each category
