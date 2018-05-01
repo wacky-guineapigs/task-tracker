d3e197296500e8fb0ccfe9b787d21e11bee60bcd
pzc <paul.zimmerman.clayton@gmail.com>
2018-05-01 09:43:38 -0500

    added db access functions as methods on manageDB.js

--- src/scripts/manageDB.js
+++ src/scripts/manageDB.js
@@ -47,33 +47,30 @@ const manageDB = Object.create({}, {
             })
             manageDB.tasks.push(newTask)
         }
+    },
+    saveTasks: {
+      value: (databaseObject, localStorageKey) => {
+        // turn DB into JSON string
+        const stringifiedDatabase = JSON.stringify(databaseObject)
+        // set to local storage
+        localStorage.setItem(localStorageKey, stringifiedDatabase)
+      },
+      retrieveTasks: {
+        value: (dbName) => {
+          // Check to see if DB exists:
+          let databaseParse = []
+          if (localStorage.getItem(dbName)) {
+            // Get the string version of the database
+            const databaseString = localStorage.getItem(dbName)
+            // Use JSON.parse() to convert the string back into an object
+            databaseParse = JSON.parse(databaseString)
+          } 
+          return databaseParse
+        }
+      }
     }
 })
 
-// Save DB to local storage:
-const saveTasksDB = (databaseObject, localStorageKey) => {
-  // turn DB into JSON string
-  const stringifiedDatabase = JSON.stringify(databaseObject)
-  // set to local storage
-  localStorage.setItem(localStorageKey, stringifiedDatabase)
-}
-
-// Function to retrieve data from local storage
-// Call using retrieveTasksDB(dbName) where dbName is the name of the string in local storage
-// String in local storage for tasks is "tasksDB"
-const retrieveTasksDB = (dbName) => {
-  // Check to see if DB exists:
-  let databaseParse = []
-  if (localStorage.getItem(dbName)) {
-    // Get the string version of the database
-    const databaseString = localStorage.getItem(dbName)
-    // Use JSON.parse() to convert the string back into an object
-    databaseParse = JSON.parse(databaseString)
-  } 
-  return databaseParse
-}
-
-saveTasksDB(manageDB.tasks, "tasks")
-console.log(retrieveTasksDB("tasks"))
+manageDB.saveTasks(manageDB.tasks, "tasks")
 
 module.exports = manageDB
