1cb8829055e7239b43a39880be6d9ccbee7f500d
pzc <paul.zimmerman.clayton@gmail.com>
2018-05-01 14:04:01 -0500

    dnd working

--- src/scripts/draganddrop.js
+++ src/scripts/draganddrop.js
@@ -4,6 +4,7 @@ const DragDropManager = Object.create(null, {
       const columns = document.querySelectorAll(".column")
       columns.forEach(column => {
         column.ondragstart = eventData => {
+          // console.log(eventData)
           // eventData is an object containing information about the object being dragged, and is created upon start of drag
           // dataTransfer is a holding area for data about the event
           eventData.dataTransfer.setData("sourceclasslist", event.target.classList)
@@ -12,10 +13,21 @@ const DragDropManager = Object.create(null, {
 
       const targets = document.querySelectorAll(".column")
       targets.forEach(target => {
+        console.log(target)
         // variable eventData is an object containing info about the ondragover event
-        target.ondragover = eventData => e.preventDefault()
-        target.ondrop = eventData => 
+        target.ondragover = eventData => eventData.preventDefault()
+        target.ondrop = eventData => {
+          console.log(eventData)
+          eventData.preventDefault()
+          // Pull from holding area info about the dragged item's class
+          const data = eventData.dataTransfer.getData("sourceclasslist")
+          eventData.target.appendChild(document.querySelector(`.${data}`))
+        }
       })
     }
   }
 })
+
+DragDropManager.init()
+
+module.exports = DragDropManager
