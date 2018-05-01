2000f6334196aacdf907f7ec59765bbfb7676156
Levi Schubert <me@levischubert.com>
2018-05-01 09:51:02 -0500

    added category add handling

--- src/scripts/formsEvent.js
+++ src/scripts/formsEvent.js
@@ -1,3 +1,6 @@
+const newCategory = require("./categories")
+const appendCategory = require("./DOMBuilder")
+
 const openTaskAdd = () => {
 	let addBox = document.querySelector(".card__form")
 	button = document.querySelector(".task__button")
@@ -18,10 +21,16 @@ const closeCatAdd = () => {
 
 const addCategory = () =>{
 	// pull value from #form__new__category
+	let catRef = document.querySelector("#form__new__category")
+	let newCat = catRef.value
 	// add it to the category array
+	newCategory.categories.push(newCat)
 	// print it to the DOM at #form__category as option with value and text content set to the pulled value
+	appendCategory.addCategoryToDom(newCat)
 	//clear text content on input
+	catRef.value = ""
 	// call 
+	closeCatAdd
 }
 
 const openCatAdd = () => {
