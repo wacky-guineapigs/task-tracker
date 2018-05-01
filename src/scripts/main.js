8c6800431ec436d7f605ee4f3df7eb3491ae7edd
Katheryn Ford <AnEvilHerbivoreLP@gmail.com>
2018-05-01 12:52:36 -0500

    Page populates properly on load

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,3 +5,11 @@ const manageDB = require("./manageDB")
 // preload categories from storage to database and task form 
 categoryDB.categories = manageDB.retrieveTasks("categories")
 DOMBuilder.addCategoryToDom(categoryDB.categories)
+
+manageDB.tasks = manageDB.retrieveTasks("tasks")
+
+taskData = manageDB.retrieveTasks("tasks")
+taskData.forEach(element => {
+    DOMBuilder.addCardToDom(element.Title, element.Category, element.Description, element.Due, element.currentStatus)
+});
+
