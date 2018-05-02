cd3d63f2c56cb6824013077b9f8156f86ffd8d04
joshdbarton <joshdbarton@gmail.com>
2018-05-01 22:46:19 -0500

    debugged data analysis functions and added charts to index.html

--- src/scripts/graphs.js
+++ src/scripts/graphs.js
@@ -48,12 +48,20 @@ const dataAnalysis = Object.create({}, {
             const onTimeTasks = manageDB.tasks.filter(task => {
                 const dueDate = task.Due
                 const completedDate = task.Completed
+                if (completedDate > 0) {
                 return dueDate >= completedDate
+                } else {
+                    return false
+                }
             })
             const lateTasks = manageDB.tasks.filter(task => {
                 const dueDate = task.Due
                 const completedDate = task.Completed
-                return dueDate <= completedDate
+                if (completedDate > 0) {
+                    return dueDate <= completedDate
+                } else {
+                    return false
+                }
             })
             const onTime = onTimeTasks.length
             const behindSchedule = lateTasks.length
@@ -71,6 +79,157 @@ const dataAnalysis = Object.create({}, {
                 const tasksinCategory = manageDB.tasks.filter(task => task.Category === category)
                 return tasksinCategory.reduce((a, b) => a + (b.Completed - b.Created), 0)/tasksinCategory.length
             })
+        }
+    }
+})
+
+var myChart = new Chart(document.getElementById("currentColumns"), {
+    type: "bar",
+    data: {
+        labels: ["to do", "doing", "done", "archived"],
+        datasets: [{
+            label: "# of tasks",
+            data: dataAnalysis.currentColumns(),
+            backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)"
+            ],
+            borderColor: [
+                "rgba(255,99,132,1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)"
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+})
+var myChart = new Chart(document.getElementById("currentCategoryTotals"), {
+    type: "bar",
+    data: {
+        labels: dataAnalysis.currentCategoryTotals("labels"),
+        datasets: [{
+            label: "# of tasks",
+            data: dataAnalysis.currentCategoryTotals("tally"),
+            backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)"
+            ],
+            borderColor: [
+                "rgba(255,99,132,1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(255,99,132,1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(255,99,132,1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)"
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+})
+
+var myChart = new Chart(document.getElementById("onTimeData"), {
+    type: "doughnut",
+    data: {
+        labels: ["on time", "late"],
+        datasets: [{
+            label: "# of completed tasks",
+            data: dataAnalysis.onTimeData(),
+            backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)"
+            ],
+            borderColor: [
+                "rgba(255,99,132,1)",
+                "rgba(54, 162, 235, 1)"
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {}
+})
+
+var myChart = new Chart(document.getElementById("categoryAverages"), {
+    type: "bar",
+    data: {
+        labels: dataAnalysis.currentCategoryTotals("labels"),
+        datasets: [{
+            label: "Average time to complete task in category",
+            data: dataAnalysis.categoryAverages(),
+            backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)"
+            ],
+            borderColor: [
+                "rgba(255,99,132,1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(255,99,132,1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(255,99,132,1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)"
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
         }
     }
 })
